a4d9eb5af63bb6893c3df9f7553cf432ca040e34
Yakir Adela <yakirade@gmail.com>
2025-05-14 12:58:33 +0300

    Update node_groups.tf

--- flask-app/infra/node_groups.tf
+++ flask-app/infra/node_groups.tf
@@ -28,6 +28,7 @@ resource "aws_iam_role_policy_attachment" "node_group_registry_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+# 🔒 Node Group בפרייבט סאבנט
 resource "aws_eks_node_group" "private_nodes" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "private-node-group"
@@ -43,4 +44,19 @@ resource "aws_eks_node_group" "private_nodes" {
   instance_types = [var.node_group_instance_type]
 }
 
+# 🌐 Node Group בפאבליק סאבנט
+resource "aws_eks_node_group" "public_nodes" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "public-node-group"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+  subnet_ids      = aws_subnet.public[*].id
+
+  scaling_config {
+    desired_size = var.node_group_desired_size
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = [var.node_group_instance_type]
+}
 
