cfd76b7363bef935fb639a1dd723a022f43c47a7
Yakir Adela <yakirade@gmail.com>
2025-05-15 21:41:16 +0300

    Delete flask-app/infra directory

--- flask-app/infra/bucket.tf
+++
@@ -1,9 +0,0 @@
-
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucketxyz123"
-
-  tags = {
-    Name = "TerraformState"
-  }
-}
--- flask-app/infra/s3_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bucketxyz123"    # שם הבקט שיצרת
-    key    = "terraform/terraform.tfstate"      # המיקום של ה-state בתוך ה-bucket
-    region = "us-east-2"                        # האזור שבו נמצא ה-S3 bucket
-    encrypt = true                              # אם אתה רוצה להצפין את הקובץ ב-S3
-  }
-}
-
