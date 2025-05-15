66168103c6dc3c612932b6a8abf86ddc8240a04e
Yakir Adela <yakirade@gmail.com>
2025-05-15 18:42:32 +0300

    Update iam.tf

--- flask-app/infra/iam.tf
+++ flask-app/infra/iam.tf
@@ -1,90 +1,96 @@
-# יצירת מדיניות IAM עבור גישה רק ל-EKS, Node Group, S3 ו-ECR
+provider "aws" {
+  region = "us-east-2"
+}
+
+# שליפת המשתמש הקיים yakir
+data "aws_iam_user" "yakir" {
+  user_name = "yakir"
+}
+
+# מדיניות מתוקנת עם כל ההרשאות הדרושות לפרויקט
 resource "aws_iam_policy" "yakir_project_policy" {
   name        = "yakir-project-policy"
-  description = "Policy to allow yakir to manage specific resources for the Terraform project"
+  description = "Policy to allow yakir to manage EKS, EC2, Node Groups, ECR, and S3 for Terraform"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      # הרשאות לגישה ל-EKS Cluster
+      # EKS Cluster
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "eks:DescribeCluster",
           "eks:ListClusters",
           "eks:UpdateClusterVersion",
           "eks:CreateCluster",
-          "eks:DeleteCluster"
-        ]
-        Resource = "*"
-      },
-      # הרשאות עבור Node Group של EKS
-      {
-        Effect   = "Allow"
-        Action   = [
+          "eks:DeleteCluster",
+          "eks:DescribeNodegroup",
           "eks:CreateNodegroup",
           "eks:UpdateNodegroupConfig",
-          "eks:DeleteNodegroup",
-          "eks:DescribeNodegroup"
+          "eks:DeleteNodegroup"
         ]
         Resource = "*"
       },
-      # הרשאות גישה ל-S3 עבור Terraform state
+      # EC2 for Node Group
       {
-        Effect   = "Allow"
-        Action   = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ]
-        Resource = "arn:aws:s3:::terraform-state-bucketxyz123/*"
-      },
-      # הרשאות גישה ל-ECR לדחיפת ודפיסת תמונות Docker
-      {
-        Effect   = "Allow"
-        Action   = [
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetAuthorizationToken",
-          "ecr:PutImage",
-          "ecr:BatchGetImage"
-        ]
-        Resource = "arn:aws:ecr:us-east-2:***:repository/flask-app-repo"
-      },
-      # הרשאות קריאה וכתיבה על EC2 עבור Node Group
-      {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "ec2:DescribeInstances",
           "ec2:CreateSecurityGroup",
           "ec2:DescribeSecurityGroups",
           "ec2:CreateLaunchTemplate",
-          "ec2:CreateSecurityGroupRule",
-          "ec2:ModifyInstanceAttribute"
+          "ec2:RunInstances",
+          "ec2:CreateTags",
+          "ec2:AuthorizeSecurityGroupIngress"
         ]
         Resource = "*"
+      },
+      # IAM PassRole (נדרש להפעלת Node Group ו-EKS)
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:GetRole",
+          "iam:PassRole"
+        ]
+        Resource = "*"
+      },
+      # ECR
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:PutImage",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      },
+      # S3 – Terraform state
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::terraform-state-bucketxyz123",
+          "arn:aws:s3:::terraform-state-bucketxyz123/*"
+        ]
       }
     ]
   })
 }
 
-# יצירת משתמש IAM בשם yakir
-resource "aws_iam_user" "yakir" {
-  name = "yakir"
-}
-
 # חיבור המדיניות למשתמש yakir
-resource "aws_iam_user_policy_attachment" "attach_yakir_project_policy" {
-  user       = aws_iam_user.yakir.name
+resource "aws_iam_user_policy_attachment" "attach_yakir_policy" {
+  user       = data.aws_iam_user.yakir.user_name
   policy_arn = aws_iam_policy.yakir_project_policy.arn
 }
 
-# יצירת Access Key עבור המשתמש yakir
-resource "aws_iam_access_key" "yakir_access_key" {
-  user = aws_iam_user.yakir.name
-}
-
 # יצירת תפקיד IAM עבור EKS Cluster
-resource "aws_iam_role" "eks_role" {
+resource "aws_iam_role" "eks_cluster_role" {
   name               = "eks-cluster-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -101,7 +107,7 @@ resource "aws_iam_role" "eks_role" {
 }
 
 # יצירת תפקיד IAM עבור Node Group
-resource "aws_iam_role" "node_group_role" {
+resource "aws_iam_role" "eks_node_group_role" {
   name               = "eks-node-group-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -118,26 +124,25 @@ resource "aws_iam_role" "node_group_role" {
 }
 
 # חיבור מדיניות לתפקיד EKS Cluster
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_role.name
+resource "aws_iam_role_policy_attachment" "attach_eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# חיבור מדיניות לתפקיד Node Group: Worker Node
-resource "aws_iam_role_policy_attachment" "node_group_worker_node_policy" {
-  role       = aws_iam_role.node_group_role.name
+# חיבור מדיניות לתפקיד Node Group – Worker Node
+resource "aws_iam_role_policy_attachment" "attach_worker_node_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-# חיבור מדיניות לתפקיד Node Group: CNI
-resource "aws_iam_role_policy_attachment" "node_group_cni_policy" {
-  role       = aws_iam_role.node_group_role.name
+# חיבור מדיניות לתפקיד Node Group – CNI
+resource "aws_iam_role_policy_attachment" "attach_cni_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-# חיבור מדיניות לתפקיד Node Group: ECR Registry Access
-resource "aws_iam_role_policy_attachment" "node_group_registry_policy" {
-  role       = aws_iam_role.node_group_role.name
+# חיבור מדיניות לתפקיד Node Group – ECR read access
+resource "aws_iam_role_policy_attachment" "attach_ecr_read_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
-
