4d2bd511c7c6eb3f27c32dc4875650f54ac491d2
Yakir Adela <yakirade@gmail.com>
2025-05-15 12:25:21 +0300

    Update main.tf

--- flask-app/infra/main.tf
+++ flask-app/infra/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.region
 }
 
+# ================= VPC =================
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -52,6 +53,7 @@ resource "aws_nat_gateway" "nat" {
   depends_on = [aws_internet_gateway.igw]
 }
 
+# ================= Route Tables =================
 resource "aws_route_table" "public_rt" {
   count  = 2
   vpc_id = aws_vpc.main.id
@@ -82,4 +84,41 @@ resource "aws_route_table_association" "private_assoc" {
   count          = 2
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt[count.index].id
+}
+
+# ================= EKS Cluster =================
+resource "aws_eks_cluster" "main" {
+  name     = "devops-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.private[*].id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_policy
+  ]
+}
+
+# ================= EKS Node Group =================
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "flask-node-group"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+  subnet_ids      = aws_subnet.private[*].id
+
+  scaling_config {
+    desired_size = var.node_group_desired_size
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = [var.node_group_instance_type]
+
+  depends_on = [
+    aws_eks_cluster.main,
+    aws_iam_role_policy_attachment.node_group_worker_node_policy,
+    aws_iam_role_policy_attachment.node_group_cni_policy,
+    aws_iam_role_policy_attachment.node_group_registry_policy
+  ]
 }
